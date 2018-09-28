4a8f3b74fa6e42a371ca7e8b805d83177c382ddc
Tilak <aruntilak2@gmail.com>
2018-09-27 21:42:44 -0700

    Dynamic maps loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
 import axios from 'axios';
+import SearchAppBar from './sidebar'
 
-
-class App extends Component {
-  
+class App extends Component { 
   state = {
-    venues : []
-  }
+     venues : []
+  } 
   componentDidMount() {
     this.getvenues()
+    this.loadMaps()
+  }
+  loadMaps =() =>{
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCf3etmckSNxbM0d_htnZZGwKTpQP2TIQk&callback=initMap')
+    window.initMap = this.initMap
+  }
+
+  initMap =() =>{
+     const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: 47.6165195,
+        lng: -122.2044366
+      },
+      zoom : 15
+    })
+
+  this.state.venues.map(myvenue => {
+
+
+    var marker = new window.google.maps.Marker({
+      position: {
+        lat: myvenue.venue.location.lat,
+        lng: myvenue.venue.location.lng
+      },
+      map: map,
+      title: myvenue.venue.name
+    })
+  })
   }
   getvenues =() =>{
     const endpoint = "https://api.foursquare.com/v2/venues/explore?";
@@ -33,88 +60,38 @@ class App extends Component {
       .catch(error => {
         console.log('Error' + error);
       })
-
-      // this.state.venues.Map(venue =>{
-      
-      // })
-
+      this.state.venues.map(myvenue =>{    
+        var marker = new window.google.Marker({
+          position: {
+            lat: myvenue.venue.location.lat , 
+            lng: myvenue.venue.location.lng
+          },
+          map: Map,
+        title: myvenue.venue.title
+        }) 
+      })
   }
   render() {
-    const style ={
-      height: '100%',
-      width:'100%',
-      margin: '0'
-    }
+    
     return (
         <div className="App">
-          <h1>Neighborhood Map</h1>
-          
-          <Map 
-            google={this.props.google} 
-            zoom={13}
-            style={style}
-            initialCenter = {{
-              lat: 47.38093,
-              lng: -122.23484
-            }}
-            >
-              {/* onMarkerClick (props, marker){
-              Marker.title ="Working?"
-            } */}
-           
-           
-            <Marker 
-              // onClick={this.onMarkerClick}
-              name={'Current location'} 
-              title= {"Tiffany & Co."}
-              position= {{
-                lat: 47.6165195,
-                lng: -122.2044366
-              }}             
-            />
-            <Marker 
-              // onClick={this.onMarkerClick}
-              name={'Current location'} 
-              title= {"Ben Bridge Jeweler"}
-              position= {{
-                lat: 47.61569136960391,
-                lng: -122.20395225712211
-              }}             
-            /><Marker 
-              // onClick={this.onMarkerClick}
-              name={'Current location'} 
-              title= {"The Bellevue Jeweler"}
-              position= {{
-                lat: 47.61445212411564,
-                lng: -122.20161437988281
-              }}             
-            /><Marker 
-              // onClick={this.onMarkerClick}
-              name={'Current location'} 
-              title= {"Nique Design Jewelers"}
-              position= {{
-                lat: 47.612116548161595,
-                lng: -122.20192996989636
-              }}             
-            /><Marker 
-              // onClick={this.onMarkerClick}
-              name={'Current location'} 
-              title= {"Gordon James Fine Diamonds"}
-              position= {{
-                lat: 47.61015585385117,
-                lng: -122.20452994108199
-              }}             
-            />
-            {/* <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                  <h1>{this.state.selectedPlace.name}</h1> 
-                </div>
-            </InfoWindow> */}
-          </Map>
+          <h1>Neighborhood Map</h1>  
+          <SearchAppBar />        
+          <div id="map">
+          </div>
         </div>
     );
   }
 }
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyCf3etmckSNxbM0d_htnZZGwKTpQP2TIQk")
-})(App)
+function loadScript(url) {
+  var index = window.document.getElementsByTagName ('script')[0]
+  var script = window.document.createElement('script')
+  script.src = url
+  script.async =true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+export default App;
+// export default GoogleApiWrapper({
+//   apiKey: ("AIzaSyCf3etmckSNxbM0d_htnZZGwKTpQP2TIQk")
+// })(App)
