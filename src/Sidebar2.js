b342b8559f84ebd2b8ffd608d0ab87dffb411b55
Tilak <aruntilak2@gmail.com>
2018-09-30 15:16:35 -0700

    Sidebar with Dynamic data Success

--- src/Sidebar2.js
+++ src/Sidebar2.js
@@ -15,31 +15,44 @@ class SideBar extends React.Component{
 
         // for local use
         var venuearray = this.props.ListOfVenues;
-        console.log(venuearray);
+        // console.log("venuarray :"+venuearray);
+        venuearray.map (venuename => {
+            {/* console.log(venuename.venue.name); */}
+            let name = venuename.venue.name;
+            console.log(name);
+            {name}
+            // <li>
+            // <h2 key={name} name={name}></h2> 
+            // </li>
+        })
 
         return(
            <div>
-            <div>
-               <h3> Search here...</h3>
-               <input id = "searchfield" type="text" name="fname" placeholder= "Search for a place..."></input>
-            </div>
-            <div>
-               <ul>
-               {
-                   venuearray.map (venuename => {
-                    {/* console.log(venuename.venue.name); */}
-                       let name = venuename.venue.name;
-                       console.log(name);
-                       <li>
-                       <h2>{name}</h2> 
-                       </li>
-                   })
-               }
-               </ul>
-               </div>
+                    <div>
+                        <h3> Search here...</h3>
+                        <input id = "searchfield" type="text" name="fname" placeholder= "Search for a place..."></input>
+                        {/* <h2 key={name} name={name}></h2>  */}
+                        {/* <h2> {venuearray[0].venue.name}</h2> */}
+
+                        <ol>
+                            {
+                                venuearray.map (venuename => {
+                                    {/* console.log(venuename.venue.name); */}
+                                    let name = venuename.venue.name;
+                                    console.log(name);
+                                    return(
+                                        <li>
+                                   {/* <h1>name</h1> */}
+                                    <h5>{name}</h5> 
+                                    </li>
+                                    )
+
+                                })
+                            }  
+                        </ol>      
+
+                    </div>
            </div>
-
-
         );
     }
 }
